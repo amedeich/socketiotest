6543ad251cb1c6e7011863da1b8d1aa0fd3ce46b
Alvaro Medina <alvaro.medina@quantomm.tech>
2021-11-17 14:14:12 -0500

    test

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const path = require('path')
-require('dotenv').config({ path: path.join(__dirname, '.env') })
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, ".env") });
 
-const Server = require('./models/server')
+const Server = require("./models/server");
 
-const server = new Server()
+const server = new Server();
 
-server.listen()
+console.log("holis");
+
+server.listen();
